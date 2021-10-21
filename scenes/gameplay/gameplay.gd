b4741c30544b9006e8111f34b9399b6ad6678398
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-21 13:11:08 -0400

    Regular auto-commit at 13:11:08 on 21/10/2021

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -2,6 +2,8 @@ extends Control
 
 onready var size = OS.window_size
 onready var maximized = false
+onready var following = false
+onready var dragging_start_position = Vector2()
 
 func _ready() -> void:
 	OS.set_window_title("Trivia Applet")
@@ -14,3 +16,17 @@ func _on_maximized_pressed():
 	else:
 		OS.window_size = size
 	maximized = !maximized
+
+
+
+func _on_Resize_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1:
+			following = !following
+			dragging_start_position = get_local_mouse_position()
+
+func _process(_delta):
+	if following:
+		print(OS.get_real_window_size(), dragging_start_position)
+
+
