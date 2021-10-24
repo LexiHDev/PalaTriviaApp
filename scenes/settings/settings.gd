8ac6c37a2e9f57c25c8b39ba31b65ff047d1f81e
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 07:29:08 -0400

    Regular auto-commit at 07:29:08 on 24/10/2021

--- scenes/settings/settings.gd
+++ scenes/settings/settings.gd
@@ -5,13 +5,22 @@ onready var size = OS.window_size
 onready var maximized = false
 onready var following = false
 onready var dragging_start_position = Vector2()
+onready var current = 'video'
+
+var settings_screens = {}
 
 
-
+func _ready():
+	var childr = $Control/HSplitContainer/bg/margins/curSetting.get_children()
+	for x in childr:
+		settings_screens[x.name] = x
+	print(settings_screens)
 func switch_to_setting(setting: String):
 	print("setting pressed:", setting)
-	pass
-
+	settings_screens[current].visible = false
+	settings_screens[setting].visible = true
+	current = setting
+	
 
 func _on_setting_pressed(setting):
 	print("pressed!")
