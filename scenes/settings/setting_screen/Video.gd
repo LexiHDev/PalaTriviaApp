786042341dabfaa00e4765d09631f893bd02ad7c
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 14:09:08 -0400

    Regular auto-commit at 14:09:08 on 24/10/2021

--- scenes/settings/setting_screen/Video.gd
+++ scenes/settings/setting_screen/Video.gd
@@ -8,7 +8,7 @@ onready var transparency = $HBoxContainer2/CheckButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	transparency.pressed = !Game._get_transparency()
+	transparency.pressed = Game._get_transparency()
 	pass # Replace with function body.
 
 
@@ -23,3 +23,4 @@ func _on_ColorPickerButton_color_changed(color):
 
 func _on_CheckButton_toggled(button_pressed):
 	Game._toggle_transparency()
+	transparency.pressed = Game._get_transparency()
