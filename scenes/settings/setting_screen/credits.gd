992916f042bad15634f16892f2cfa934ed4359ed
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 09:09:08 -0400

    Regular auto-commit at 09:09:08 on 24/10/2021

--- scenes/settings/setting_screen/credits.gd
+++ scenes/settings/setting_screen/credits.gd
@@ -4,13 +4,14 @@ extends ScrollContainer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+var auto_scrolling = true
+var old_scroll = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	scroll_vertical += 1
+	if scroll_vertical == old_scroll:
+		scroll_vertical = 0
