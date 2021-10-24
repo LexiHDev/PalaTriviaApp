19ccf378a0e791f27a3cd5410d24fea51d4377b4
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 09:19:08 -0400

    Regular auto-commit at 09:19:08 on 24/10/2021

--- scenes/settings/setting_screen/credits.gd
+++ scenes/settings/setting_screen/credits.gd
@@ -5,13 +5,22 @@ extends ScrollContainer
 # var a = 2
 # var b = "text"
 var auto_scrolling = true
+var rev_scroll = false
 var old_scroll = 0
+var tween = Tween.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	add_child(tween)
+	tween.interpolate_property(
+		self, "scroll_veritcal", 
+		old_scroll, 0, 1,
+		Tween.TRANS_QUAD, Tween.EASE_IN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll_vertical += 1
+	scroll_vertical += 1 if !rev_scroll else -20
 	if scroll_vertical == old_scroll:
-		scroll_vertical = 0
+		rev_scroll = true
+	elif scroll_vertical == 0:
+		rev_scroll = false
+	old_scroll = scroll_vertical
