6c440bc3f86836d5131fba39816869aa93a1806b
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 08:09:08 -0400

    Regular auto-commit at 08:09:08 on 24/10/2021

--- scenes/settings/setting_screen/RichTextLabel.gd
+++ scenes/settings/setting_screen/RichTextLabel.gd
@@ -9,11 +9,8 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var bbc = File.new()
-	bbc.open("res://assets/credits.bbcode", File.READ)
-	self.bbcode_text = bbc.get_as_text()
-	bbc.close()
-
+	var bbcode = load("res://assets/credits.tres")
+	bbcode_text = bbcode
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
