2bc742749c6d1268aca96744d0aa75f8c6e9bcba
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 08:19:08 -0400

    Regular auto-commit at 08:19:07 on 24/10/2021

--- scenes/settings/setting_screen/RichTextLabel.gd
+++ scenes/settings/setting_screen/RichTextLabel.gd
@@ -5,12 +5,21 @@ extends RichTextLabel
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+func load_text_file(path):
+	var f = File.new()
+	var err = f.open(path, File.READ)
+	if err != OK:
+		printerr("Could not open file, error code ", err)
+		return ""
+	var text = f.get_as_text()
+	f.close()
+	return text
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var bbcode = load("res://assets/credits.tres")
-	bbcode_text = bbcode
+	var text = load_text_file("res://assets/credits.txt")
+	bbcode_text = text
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
