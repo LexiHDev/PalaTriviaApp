b1d02f4ebb0ba82e9254f2956ace39f84154f9ba
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-24 09:29:08 -0400

    Regular auto-commit at 09:29:08 on 24/10/2021

--- scenes/settings/setting_screen/credits.gd
+++ scenes/settings/setting_screen/credits.gd
@@ -8,8 +8,11 @@ var auto_scrolling = true
 var rev_scroll = false
 var old_scroll = 0
 var tween = Tween.new()
+var vscroll
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	vscroll = get_child(1)
+	vscroll.connect("scrolling", self, "_toggle_scroll")
 	add_child(tween)
 	tween.interpolate_property(
 		self, "scroll_veritcal", 
@@ -18,9 +21,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll_vertical += 1 if !rev_scroll else -20
-	if scroll_vertical == old_scroll:
-		rev_scroll = true
-	elif scroll_vertical == 0:
-		rev_scroll = false
-	old_scroll = scroll_vertical
+	if auto_scrolling:
+		scroll_vertical += 1.5 if !rev_scroll else -20
+		if scroll_vertical == old_scroll:
+			rev_scroll = true
+		elif scroll_vertical == 0:
+			rev_scroll = false
+		old_scroll = scroll_vertical
+
+
+
+func _toggle_scroll():
+	print("controll scroll")
+	auto_scrolling = false
